945cf2b21d7ea65f7b7d9f048f173db6418250d8
Andrew Wilson <AndrewW@agilebridge.co.za>
2021-07-02 10:44:59 +0200

    Running changes

--- NaughtiesAndCrosses/tabs/src/components/sample/Graph.jsx
+++ NaughtiesAndCrosses/tabs/src/components/sample/Graph.jsx
@@ -14,6 +14,7 @@ export function Graph() {
       } catch {
         // Could not fetch photo from user's profile, return empty string as placeholder.
       }
+      console.log(profile.id);
       return { profile, photoUrl };
     },
     { scope: ["User.Read"] }
